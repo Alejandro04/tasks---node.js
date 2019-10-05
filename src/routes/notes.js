bb015dffe1e113d886fefd95d26f411ff05452b2
Alejandro Roa <a.roa@fixme.es>
2019-10-05 17:34:10 -0500

    add bootstrap for alert - add method post validation

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -10,6 +10,26 @@ router.get('/notes/add', (req, res) => {
     res.render('notes/add')
 })
 
+router.post('/notes/add', (req, res) => {
+    const { title, description } = req.body;
+    const errors = [];
+    if (!title) {
+      errors.push({text: 'Ingrese el titulo'});
+    }
+    if (!description) {
+      errors.push({text: 'Ingrese la descripción'});
+    }
+    if (errors.length > 0) {
+      res.render('notes/add', {
+        errors,
+        title,
+        description
+      });
+    } else {
+      res.send("#vamos bien")
+    }
+})
+
 router.get('/notes/edit', (req, res) => {
     res.render('notes/edit')
 })
