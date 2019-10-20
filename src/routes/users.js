65bcbd404eed50006421082b362c19a3529d63f6
Alejandro Roa <a.roa@fixme.es>
2019-10-19 21:11:55 -0500

    add passport local, auth

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,44 +1,52 @@
 const express = require('express');
+const passport = require('passport');
 const router = express.Router();
 
 // Models
 const User = require('../models/User');
 
 router.get('/users/signin', (req, res) => {
-    res.render('users/signin')
+  res.render('users/signin')
 })
 
 router.get('/users/signup', (req, res) => {
-    res.render('users/signup')
+  res.render('users/signup')
 })
 
 router.post('/users/signup', async (req, res) => {
-    let errors = [];
-    const { name, email, password, confirm_password } = req.body;
+  let errors = [];
+  const { name, email, password, confirm_password } = req.body;
 
-    if(password != confirm_password) {
-      errors.push({text: 'Los passwords no coinciden'});
-      console.log("Los passwords no coinciden")
-    }
-    if(password.length < 5) {
-      errors.push({text: 'El password debe tener mínimo 5 caracteres'})
-      console.log("El password debe tener mínimo 5 caracteres")
-    }
-    if(errors.length > 0){
-      res.render('users/signup', {errors, name, email, password, confirm_password});
+  if (password != confirm_password) {
+    errors.push({ text: 'Los passwords no coinciden' });
+    console.log("Los passwords no coinciden")
+  }
+  if (password.length < 5) {
+    errors.push({ text: 'El password debe tener mínimo 5 caracteres' })
+    console.log("El password debe tener mínimo 5 caracteres")
+  }
+  if (errors.length > 0) {
+    res.render('users/signup', { errors, name, email, password, confirm_password });
+  } else {
+    const emailUser = await User.findOne({ email: email });
+    if (emailUser) {
+      req.flash('error_msg', 'El email ya está en uso');
+      res.redirect('/users/signup');
     } else {
-      const emailUser = await User.findOne({email: email});
-      if(emailUser) {
-        req.flash('error_msg', 'El email ya está en uso');
-        res.redirect('/users/signup');
-      } else {
-        const newUser = new User({name, email, password});
-        newUser.password = await newUser.encryptPassword(password);
-        await newUser.save();
-        req.flash('success_msg', 'Registro exitoso');
-        res.redirect('/users/signin');
-      }
+      const newUser = new User({ name, email, password });
+      newUser.password = await newUser.encryptPassword(password);
+      await newUser.save();
+      req.flash('success_msg', 'Registro exitoso');
+      res.redirect('/users/signin');
     }
-  });
+  }
+});
+
+router.post('/users/signin', passport.authenticate('local', {
+  successRedirect: '/notes',
+  failureRedirect: '/users/signin',
+  failureFlash: true
+}));
+
 
 module.exports = router;
